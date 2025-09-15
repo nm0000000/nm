7526e4967595ef0a98f06d3dd89e8a4e937a06c0
nm0000000 <anastasia.zh32@gmail.com>
2025-09-15 21:24:43 +0300

    Create listpy.py

---
+++ listpy.py
@@ -0,0 +1,9 @@
+#cохраним список в переменной
+colors = ['black', 'white', 'red', 'orange']
+
+#организация стека 
+stack =
+stack.append('black')
+stack.append('white')
+stack.append('red')
+stack.append('orange')
