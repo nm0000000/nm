5fd68dd6bc499fb0dadb05d5e9f052bc57aefdaf
nm0000000 <anastasia.zh32@gmail.com>
2025-09-15 21:27:43 +0300

    Create listjava.java

---
+++ listjava.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+ 
+public class Program{
+      
+    public static void main(String[] args) {
+          
+        ArrayList<String> colors = new ArrayList<String>();
+        // добавим в список ряд элементов
+        people.add("black");
+        people.add("white");
+        people.add("red");
+        people.add("orange");
+        
+    }
+
+    //cоздание стек
+
+    List<String> stack = new ArrayList<>()
+
+    stack.add("black");
+    stack.add("white");
+    stack.add("red");
+    stack.add("orange");
+    System.out.println(stack);
+
+}
